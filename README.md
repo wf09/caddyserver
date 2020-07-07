46fee06ced4f0dbf2842604e5de4c1162f72a7cc
wf09 <wf.09@qq.com>
2020-07-07 08:56:38 +0800

    update

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 caddy一键安装管理脚本
 
 ```bash
-https://fly97.coding.net/p/shell/d/shell/git/raw/master/caddyserver/caddy.sh
+wget https://cdn.jsdelivr.net/gh/wf09/caddyserver/caddy.sh
 chmod +x caddy.sh
 ./caddy.sh
 ```
+
--- caddy.sh
+++ caddy.sh
@@ -13,7 +13,7 @@ fi
 echo ""
 echo "  Setting up Caddy"
 cd /tmp
-wget -q https://fly97.coding.net/p/shell/d/shell/git/raw/master/caddyserver/caddy
+wget -q https://cdn.jsdelivr.net/gh/wf09/caddyserver/caddy
 chmod +x caddy; mv caddy /usr/local/bin;
 echo "  Done. run 'caddy' to use Caddy"
 echo ""
